c801965e7bf7b515c1ac1e4e8e64a5bdbf94058d
Axout <rako-serge@ya.ru>
2020-12-03 11:20:27 +0300

    Initial commit

---
+++ app/src/main/java/ru/axout/roomdatabasedemo/MainDao.java
@@ -0,0 +1,33 @@
+package ru.axout.roomdatabasedemo;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+import static androidx.room.OnConflictStrategy.REPLACE;
+
+@Dao
+public interface MainDao {
+    // Вставка запроса
+    @Insert(onConflict = REPLACE)
+    void insert(MainData mainData);
+
+    // Удаление запроса
+    @Delete
+    void delete(MainData mainData);
+
+    // Удаление всех запросов
+    @Delete
+    void reset(List<MainData> mainData);
+
+    // Обновление запросов
+    @Query("UPDATE table_name SET text = :sText WHERE ID = :sID")
+    void update(int sID, String sText);
+
+    // Выдача всех запросов
+    @Query("SELECT * FROM table_name")
+    List<MainData> getAll();
+}
